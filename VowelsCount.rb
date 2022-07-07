372c499df275b1b8c90576c3ee3ef02f90ffd9cd
Chreees79 <christelle.gevaert32@gmail.com>
2022-07-08 00:15:57 +0200

    add first Katas For Friday

---
+++ VowelsCount.rb
@@ -0,0 +1,4 @@
+def get_count(input_str)
+  input_str.count("aeiou")
+end
+puts get_count("Salut les copains")
---
+++ remove_smallest.rb
@@ -0,0 +1,6 @@
+def remove_smallest(numbers)
+
+  numbers.reject.with_index { |k,i| i == numbers.index(numbers.min) }
+  
+  end
+print remove_smallest([1,1,1,2,6,8,8,7,5,2])
